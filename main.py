0bab390d9c58a78d6a6081bb8e18fc3284cd8b9d
turtlecode <85156399+turtlecode@users.noreply.github.com>
2022-02-28 16:54:33 +0300

    How to set enabled button - Python #PyQT6
    How to set enabled button - Python #PyQT6

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        self.button = QPushButton("Press Me!")
+        self.button.clicked.connect(self.the_button_was_clicked)
+
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        self.button.setText("You already clicked me.")
+        self.button.setEnabled(False)
+        self.setWindowTitle("My Oneshot App")
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
